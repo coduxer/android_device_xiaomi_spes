be5912e47c42c99ab087c090262aa6a1d29a8203
Jabiyeff <cebiyevanar@gmail.com>
2022-07-03 23:06:54 +0400

    remove telephony-ext
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -468,11 +468,7 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+    qti_telephony_utils.xml
 
 # Update Engine
 PRODUCT_PACKAGES += \
